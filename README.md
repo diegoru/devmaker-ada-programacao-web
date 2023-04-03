b307a526ba1ab203bed46a218976e5027f055de0
Diego <diego.ruescas@gmail.com>
2023-04-03 20:33:33 -0300

    estrutura projeto

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+![](https://i.postimg.cc/3JKXXYzt/bnn-adabook.jpg)
+
 # Projeto Final Programação Web II - DevMaker 2
 
 Criação de uma API de livraria, consumindo a Google Books API.
+
+
+
+
+![](https://i.postimg.cc/NjvwJC7C/uml-adabook.jpg)
---
+++ adabook/src/main/java/br/com/ada/adabook/AdabookApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.adabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdabookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdabookApplication.class, args);
+	}
+
+}
